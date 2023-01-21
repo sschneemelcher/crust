0be0edfcfd4ced3985a132814c82381e5824e74f
sschneemelcher <schneemelchercom@gmail.com>
2023-01-21 18:12:45 +0100

    improve the code

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,39 @@
 use std::io::stdin;
-use std::process::{Command, Child};
+use std::process::{Child, Command};
 
 fn main() {
     loop {
         let mut input = String::new();
 
-        stdin()
-        .read_line(&mut input)
-        .expect("expected a line");
-        let mut iter = input.split_whitespace();
+        stdin().read_line(&mut input).expect("expected a line");
+        let mut words = input.split_whitespace();
 
-        match iter.next() {                             // iterate over the input word by word
+        match words.next() {
+            // match the first word of the input
+            None => {}
+            Some("exit") => {
+                return;
+            }
+            Some(command) => {
+                let mut cmd = Command::new(command);
+                for word in words {
+                    // iterate over the arguments
+                    cmd.arg(word);
+                }
+                match cmd.spawn() {
+                    // spawn the processed command
+                    Ok(Child {
+                        stdin: _,
+                        mut stdout,
+                        stderr: _,
+                        ..
+                    }) => match stdout.take() {
+                        None => {}
+                        Some(output) => println!("{:#?}", output),
+                    },
 
-            None => {},                                 // if the input is empty, do nothing 
-            word => {
-                let command = word.unwrap();
-                if command == "exit" {
-                    return;
-                } else {
-                    let mut cmd = Command::new(command);
-                    for s in iter {
-                        cmd.arg(s);                         // iterate over the arguments
-                    }
-                    match cmd.spawn() {                     // spawn the processed command
-                        Ok(Child{stdin: _, mut stdout, stderr: _, ..}) => {
-                            match stdout.take() {  
-                                None => {},                 // if there is no output, do nothing
-                                x => println!("{:#?}", x.unwrap()) // else print the output
-                            }
-                        },
-                        Err(e) => println!("{:#?}", e)      // if spawning failed, print message
-                    } 
+                    // if spawning failed, print message
+                    Err(e) => println!("{:#?}", e),
                 }
             }
         }
