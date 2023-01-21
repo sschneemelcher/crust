6a165405a50087e120598ab2d514b33d29bee87e
sschneemelcher <schneemelchercom@gmail.com>
2023-01-21 20:31:04 +0100

    add basic PS2 prompt

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,14 @@ enum Builtins {
 }
 
 fn print_prompt() {
+    let prompt = match std::env::var("PS2") {
+        Ok(val) => val,
+        Err(_) => "$ ".to_string(),
+    };
+
     let mut lock = stdout().lock();
-    write!(lock, "$ ").unwrap();
+    write!(lock, "{}", prompt).unwrap();
+
     match lock.flush() {
         Ok(_) => {}
         Err(e) => println!("{:#?}", e),
