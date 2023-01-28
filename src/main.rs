6fecffc5376b3d6dd6a0e8ebc395248c942302c8
sschneemelcher <schneemelchercom@gmail.com>
2023-01-28 19:40:11 +0100

    add command chaining

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::process::{exit, Command};
 
 const SHELL_NAME: &str = "crust";
 
+#[derive(Clone)]
 struct Input {
     command: String,
     args: Vec<String>,
@@ -12,6 +13,7 @@ struct Input {
     builtin: Builtins,
 }
 
+#[derive(Clone)]
 enum Builtins {
     None,
     Exit,
@@ -26,11 +28,12 @@ fn main() {
 
         let mut input_buf = String::new();
         stdin().read_line(&mut input_buf).expect("expected a line");
-        let input: Input = parse_input(input_buf);
-
-        match input.builtin {
-            Builtins::None => execute_command(input),
-            _ => execute_builtin(input),
+        let inputs: &Vec<Input> = &parse_input(input_buf);
+        for input in inputs {
+            match input.builtin {
+                Builtins::None => execute_command(input),
+                _ => execute_builtin(input),
+            }
         }
     }
 }
@@ -50,42 +53,52 @@ fn print_prompt() {
     }
 }
 
-fn parse_input(line: String) -> Input {
-    let mut parsed_input = Input {
-        command: "".to_string(),
-        args: vec![],
-        builtin: Builtins::None,
-        bg: false,
-    };
+fn parse_input(raw_input: String) -> Vec<Input> {
+    let mut inputs: Vec<Input> = vec![];
 
-    let mut words = line.split_whitespace();
+    // Parse input line by line
+    for line in raw_input.split(['\n', ';']) {
+        if line.len() < 1 {
+            continue;
+        };
 
-    // parse the first word of the input
-    match words.next() {
-        // match the first word of the input
-        None => return parsed_input,
-        Some("exit") => {
-            parsed_input.builtin = Builtins::Exit;
-            return parsed_input;
+        let mut parsed_input = Input {
+            command: "".to_string(),
+            args: vec![],
+            builtin: Builtins::None,
+            bg: false,
+        };
+
+        let mut words = line.split_whitespace();
+
+        // parse the first word of the input
+        match words.next() {
+            // match the first word of the input
+            None => inputs.push(parsed_input.clone()),
+            Some("exit") => {
+                parsed_input.builtin = Builtins::Exit;
+                inputs.push(parsed_input.clone());
+            }
+            Some("cd") => parsed_input.builtin = Builtins::CD,
+            Some("echo") => parsed_input.builtin = Builtins::Echo,
+            Some("alias") => parsed_input.builtin = Builtins::Alias,
+            Some(command) => {
+                parsed_input.command = command.to_string();
+            }
         }
-        Some("cd") => parsed_input.builtin = Builtins::CD,
-        Some("echo") => parsed_input.builtin = Builtins::Echo,
-        Some("alias") => parsed_input.builtin = Builtins::Alias,
-        Some(command) => {
-            parsed_input.command = command.to_string();
+
+        for word in words {
+            match word {
+                "&" => parsed_input.bg = true,
+                arg => parsed_input.args.push(arg.to_string()),
+            }
         }
+        inputs.push(parsed_input);
     }
-
-    for word in words {
-        match word {
-            "&" => parsed_input.bg = true,
-            arg => parsed_input.args.push(arg.to_string()),
-        }
-    }
-    return parsed_input;
+    return inputs;
 }
 
-fn execute_command(input: Input) {
+fn execute_command(input: &Input) {
     let mut cmd = Command::new(&input.command);
     cmd.args(&input.args);
 
@@ -107,7 +120,7 @@ fn execute_command(input: Input) {
     }
 }
 
-fn execute_builtin(input: Input) {
+fn execute_builtin(input: &Input) {
     match input.builtin {
         Builtins::Exit => exit(0),
         Builtins::Echo => println! {"{}", input.args.join(" ")},
@@ -117,7 +130,7 @@ fn execute_builtin(input: Input) {
     }
 }
 
-fn change_dir(input: Input) {
+fn change_dir(input: &Input) {
     match current_dir() {
         Ok(mut path) => {
             // Check if the user has given 0 (~) or 1 arg
@@ -131,7 +144,10 @@ fn change_dir(input: Input) {
                 },
                 1 => {
                     let arg: &str = &input.args[0].clone();
-                    <PathBuf as Extend<String>>::extend::<Vec<String>>(&mut path, input.args);
+                    <PathBuf as Extend<String>>::extend::<Vec<String>>(
+                        &mut path,
+                        input.args.clone(),
+                    );
                     match set_current_dir(path) {
                         Ok(_) => {}
                         Err(_) => {
