0ffbc4b6f67fb18d3a4b915c8958656039a3f8ea
Simon Schneemelcher <schneemelchercom@gmail.com>
2023-02-14 20:19:09 +0100

    work on line editing

--- src/ui.rs
+++ src/ui.rs
@@ -1,32 +1,84 @@
 use crossterm::{
-    cursor::MoveLeft,
+    cursor::{MoveLeft, MoveRight},
     event::{read, Event, KeyCode, KeyModifiers},
     execute,
     style::Print,
-    terminal::{disable_raw_mode, enable_raw_mode},
+    terminal::{disable_raw_mode, enable_raw_mode, Clear},
     Result,
 };
-use std::{io::Stdout, process::exit};
+use std::{convert::TryInto, io::Stdout, process::exit};
+
+fn exit_raw_mode() {
+    match disable_raw_mode() {
+        Ok(()) => {}
+        Err(_) => panic! {"unable to exit raw mode"},
+    };
+}
 
 pub fn handle_keys(stdout: &mut Stdout) -> Result<String> {
+    let mut position = 0;
     let mut input: String = "".to_string();
-    enable_raw_mode().ok();
+    match enable_raw_mode() {
+        Ok(()) => {}
+        Err(_) => panic! {"unable to enter raw mode"},
+    }
     loop {
         // `read()` blocks until an `Event` is available
         match read()? {
             Event::Key(event) => match event.code {
                 KeyCode::Char(c) => {
-                    if (c == 'c' || c == 'd') && event.modifiers == KeyModifiers::CONTROL {
-                        disable_raw_mode().ok();
+                    if c == 'd' && event.modifiers == KeyModifiers::CONTROL {
+                        exit_raw_mode();
                         exit(0);
+                    } else if c == 'c' && event.modifiers == KeyModifiers::CONTROL {
+                        input = "".to_string();
+                        break;
                     }
-                    execute!(stdout, Print(c)).ok();
-                    input.push(c);
+                    match execute!(stdout, Print(c)) {
+                        Ok(()) => {
+                            input.push(c);
+                            position += 1;
+                        }
+                        Err(_) => {}
+                    }
                 }
                 KeyCode::Backspace => {
-                    if input.len() > 0 {
-                        input.pop();
-                        execute!(stdout, MoveLeft(1), Print(' '), MoveLeft(1)).ok();
+                    if input.len() > 0 && position > 0 {
+                        if input.len() == position {
+                            // delete character at end of line
+                            match execute!(stdout, MoveLeft(1), Print(' '), MoveLeft(1)) {
+                                Ok(()) => {
+                                    input.pop();
+                                    position -= 1;
+                                }
+                                Err(_) => {}
+                            }
+                        } else {
+                            // delete character from inside the line
+                            execute!(
+                                stdout,
+                                MoveLeft(position.try_into().unwrap()),
+                                Clear(crossterm::terminal::ClearType::FromCursorDown),
+                                Print(input)
+                            )
+                            .ok();
+                        }
+                    }
+                }
+                KeyCode::Left => {
+                    if position > 0 {
+                        position = match execute!(stdout, MoveLeft(1)) {
+                            Ok(()) => position - 1,
+                            Err(_) => position,
+                        };
+                    }
+                }
+                KeyCode::Right => {
+                    if position < input.len() {
+                        position = match execute!(stdout, MoveRight(1)) {
+                            Ok(()) => position + 1,
+                            Err(_) => position,
+                        };
                     }
                 }
                 KeyCode::Enter => break,
@@ -35,7 +87,7 @@ pub fn handle_keys(stdout: &mut Stdout) -> Result<String> {
             _ => {}
         };
     }
-    disable_raw_mode().ok();
+    exit_raw_mode();
     execute!(stdout, Print('\n')).ok();
     Ok(input)
 }
