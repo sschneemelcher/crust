761aa4b43e84cfd38a2c6af41c74cf1ae14bba9e
Simon Schneemelcher <schneemelchercom@gmail.com>
2023-03-10 14:01:19 +0100

    refactor ui

--- src/ui.rs
+++ src/ui.rs
@@ -1,12 +1,18 @@
 use crossterm::{
-    cursor::{MoveLeft, MoveRight, MoveToColumn, RestorePosition, SavePosition},
-    event::{read, Event, KeyCode, KeyModifiers},
+    cursor::MoveToColumn,
+    event::{read, Event, KeyCode, KeyEvent, KeyModifiers},
     execute, queue,
     style::Print,
-    terminal::{disable_raw_mode, enable_raw_mode, Clear},
+    terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType::FromCursorDown},
     Result,
 };
-use std::{convert::TryInto, env::var, fs, io::Stdout, process::exit};
+use std::{
+    convert::TryInto,
+    env::var,
+    fs,
+    io::{Stdout, Write},
+    process::exit,
+};
 
 fn exit_raw_mode() {
     match disable_raw_mode() {
@@ -15,115 +21,118 @@ fn exit_raw_mode() {
     };
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug, Default, PartialEq)]
 pub enum Mode {
-    Shell,
-    Ask,
+    #[default]
+    Input,
+    Submit,
+    Break,
+    Exit,
+}
+
+#[derive(Clone, Default, Debug)]
+pub struct Prompt {
+    position: usize,
+    input: String,
+    mode: Mode,
+    completions: Vec<String>,
+}
+
+fn handle_keypressed(event: KeyEvent, prompt: &mut Prompt) {
+    if event.modifiers == KeyModifiers::CONTROL && prompt.mode == Mode::Input {
+        match event.code {
+            KeyCode::Char('c') => {
+                prompt.mode = Mode::Break;
+                return;
+            }
+            KeyCode::Char('d') => {
+                prompt.mode = Mode::Exit;
+                return;
+            }
+            _ => {}
+        }
+    }
+
+    match event.code {
+        KeyCode::Char(c) => {
+            if prompt.position < prompt.input.len() {
+                let line = prompt.input.clone();
+                let (head, tail) = line.split_at(prompt.position);
+                prompt.input = format!("{}{}{}", head, c, tail);
+                prompt.position += 1;
+            } else {
+                prompt.input.push(c);
+                prompt.position += 1;
+            }
+        }
+        KeyCode::Backspace => {
+            if prompt.input.len() > 0 && prompt.position > 0 {
+                if prompt.input.len() == prompt.position {
+                    // delete character at end of line
+                    prompt.input.pop();
+                    prompt.position -= 1;
+                } else {
+                    // delete character from inside the line
+                    let line = prompt.input.clone();
+                    let (head, tail) = line.split_at(prompt.position - 1);
+                    prompt.input = head.to_string() + &tail[1..];
+                    prompt.position -= 1;
+                }
+            }
+        }
+        KeyCode::Left => {
+            if prompt.position > 0 {
+                prompt.position = prompt.position - 1;
+            }
+        }
+        KeyCode::Right => {
+            if prompt.position < prompt.input.len() {
+                prompt.position = prompt.position + 1;
+            }
+        }
+        KeyCode::Enter => prompt.mode = Mode::Submit,
+
+        KeyCode::Tab => {
+            // Handle completions
+            prompt.completions = get_completion(&prompt.input);
+        }
+        _ => {}
+    }
 }
 
 pub fn handle_keys(stdout: &mut Stdout) -> Result<String> {
-    let mut position = 0;
-    let mut input: String = "".to_string();
-
     match enable_raw_mode() {
         Ok(()) => {}
         Err(_) => panic! {"unable to enter raw mode"},
     }
-    let mut mode = Mode::Shell;
+
+    let mut prompt = Prompt::default();
+    print_prompt(stdout, &prompt);
 
     loop {
-        print_prompt(stdout, &mode, &input, &position);
-
         // `read()` blocks until an `Event` is available
         match read()? {
-            Event::Key(event) => match event.code {
-                KeyCode::Char(c) => {
-                    if c == 'd' && event.modifiers == KeyModifiers::CONTROL {
-                        exit_raw_mode();
-                        exit(0);
-                    } else if c == 'c' && event.modifiers == KeyModifiers::CONTROL {
-                        match mode {
-                            Mode::Shell => {
-                                input = "".to_string();
-                                break;
-                            }
-                            Mode::Ask => mode = Mode::Shell,
-                        }
-                    } else if c == 'a' && event.modifiers == KeyModifiers::CONTROL {
-                        mode = Mode::Ask;
-                    } else if position < input.len() {
-                        let line = input.clone();
-                        let (head, tail) = line.split_at(position);
-                        input = format!("{}{}{}", head, c, tail);
-                        position += 1;
-                    } else {
-                        input.push(c);
-                        position += 1;
-                    }
-                }
-                KeyCode::Backspace => {
-                    if input.len() > 0 && position > 0 {
-                        if input.len() == position {
-                            // delete character at end of line
-                            input.pop();
-                            position -= 1;
-                        } else {
-                            // delete character from inside the line
-                            let line = input.clone();
-                            let (head, tail) = line.split_at(position - 1);
-                            input = head.to_string() + &tail[1..];
-                            position -= 1;
-                        }
-                    }
-                }
-                KeyCode::Left => {
-                    if position > 0 {
-                        position = position - 1;
-                    }
-                }
-                KeyCode::Right => {
-                    if position < input.len() {
-                        position = position + 1;
-                    }
-                }
-                KeyCode::Enter => break,
-                KeyCode::Tab => {
-                    // Handle completions
-                    let completions = get_completion(&input);
-
-                    if completions.len() > 1 {
-                        queue!(
-                            stdout,
-                            SavePosition,
-                            MoveToColumn(0),
-                            Clear(crossterm::terminal::ClearType::FromCursorDown)
-                        )?;
-
-                        for completion in completions {
-                            queue!(stdout, Print(completion + "    "))?;
-                        }
-
-                        execute!(stdout, Print("\n"), MoveToColumn(0))?;
-                        print_prompt(stdout, &mode, &input, &position);
-                        execute!(stdout, Print(input.clone()), RestorePosition)?;
-                    } else if completions.len() == 1 {
-                        match execute!(stdout, Print(completions[0].clone())) {
-                            Ok(()) => {
-                                input.push_str(completions[0].as_ref());
-                                position += completions[0].len();
-                            }
-                            Err(_) => {}
-                        }
-                    }
-                }
-                _ => {}
-            },
+            Event::Key(event) => handle_keypressed(event, &mut prompt),
             _ => {}
-        };
+        }
+
+        print_prompt(stdout, &prompt);
+
+        match prompt.mode {
+            Mode::Exit => {
+                exit_raw_mode();
+                exit(0);
+            }
+            Mode::Submit => {
+                exit_raw_mode();
+                return Ok(prompt.input);
+            }
+            Mode::Break => {
+                prompt = Prompt::default();
+            }
+            _ => {}
+        }
     }
-    exit_raw_mode();
-    execute!(stdout, Print('\n')).ok();
-    Ok(input)
 }
 
 fn get_completion(_line: &str) -> Vec<String> {
@@ -140,21 +149,56 @@ fn get_completion(_line: &str) -> Vec<String> {
     return completions;
 }
 
-pub fn print_prompt(stdout: &mut Stdout, mode: &Mode, input: &str, position: &usize) {
-    let prompt = match mode {
-        Mode::Shell => match var("PS2") {
-            Ok(val) => val,
-            Err(_) => "$ ".to_string(),
-        },
-        Mode::Ask => "[ask-crust]: ".to_string(),
+pub fn print_prompt(stdout: &mut Stdout, prompt: &Prompt) {
+    match prompt.mode {
+        Mode::Submit => queue!(stdout, Print('\n')).ok().unwrap(),
+        Mode::Break => queue!(stdout, Print("^C\n")).ok().unwrap(),
+        _ => {}
+    }
+
+    queue!(stdout, MoveToColumn(0), Clear(FromCursorDown)).ok();
+
+    if prompt.mode == Mode::Submit {
+        stdout.flush().ok();
+        return;
+    }
+
+    let ps2 = match var("PS2") {
+        Ok(val) => val,
+        Err(_) => "$ ".to_string(),
     };
 
-    execute!(
-        stdout,
-        MoveToColumn(0),
-        Clear(crossterm::terminal::ClearType::FromCursorDown),
-        Print(prompt.clone() + input),
-        MoveToColumn((prompt.len() + position).try_into().unwrap())
-    )
-    .ok();
+    queue!(stdout, Print(ps2.clone())).ok();
+
+    if prompt.mode == Mode::Input {
+        queue!(stdout, Print(prompt.input.clone())).ok();
+    }
+
+    let pos: u16 = (ps2.len() + prompt.position).try_into().unwrap();
+    execute!(stdout, MoveToColumn(pos)).ok();
+
+    // if completions.len() > 1 {
+    //     queue!(
+    //         stdout,
+    //         SavePosition,
+    //         MoveToColumn(0),
+    //         Clear(crossterm::terminal::ClearType::FromCursorDown)
+    //     )?;
+
+    //     for completion in completions {
+    //         queue!(stdout, Print(completion + "    "))?;
+    //     }
+
+    //     execute!(stdout, Print("\n"), MoveToColumn(0))?;
+    //     print_prompt(stdout, &mode, &input, &position);
+    //     execute!(stdout, Print(input.clone()), RestorePosition)?;
+    // } else if completions.len() == 1 {
+    //     match execute!(stdout, Print(completions[0].clone())) {
+    //         Ok(()) => {
+    //             input.push_str(completions[0].as_ref());
+    //             position += completions[0].len();
+    //         }
+    //         Err(_) => {}
+    //     }
+    // }
 }
